ecb5bf3304ade945ba85e190cd4bd38838da2b2b
Miguel <chanampamiguel@gmail.com>
2022-08-26 18:01:39 -0300

    Problema 2

--- Problema02/Program.cs
+++ Problema02/Program.cs
@@ -11,8 +11,14 @@ namespace Problema02
 
             num1 = 30;
             num2 = 60;
-            result = Convert.ToByte(num1 * num2);
-            Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+            try {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+            } catch (System.OverflowException) {
+                Console.WriteLine("No se puede convertir el número");
+            }
+
+
             Console.ReadLine();
         }
     }
